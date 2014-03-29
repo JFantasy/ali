0c3c2e5dab641cc98a405928a1aad810db683673
Zeyu Chen <zeyuchen@outlook.com>
2014-03-29 11:32:25 +0800

    delete useless files

--- pop/src/cal_topk.py
+++ pop/src/cal_topk.py
@@ -36,38 +36,6 @@ def cal_user_buyrate(data):
 		buyrate[user] = int(buycnt[user][5])
 	
 	return buyrate		
-	# f = open('../data/buy_month_stat.txt', 'w')
-	# for user in buycnt:
-	# 	f.write(user + ' ')
-	# 	for i in range(5):
-	# 		f.write(str(i+4) + ':' + str(buycnt[user][i]) + ' ')
-	# 	f.write('\n')
-	
-	# f.close()
-
-	#min_max normalization
-	# min_tot = 10000
-	# max_tot = 0
-	# min_buy = 10000
-	# max_buy = 0
-	# for user in buycnt:
-	# 	min_tot = min(min_tot, buycnt[user][1])
-	# 	max_tot = max(max_tot, buycnt[user][1])
-	# 	min_buy = min(min_buy, buycnt[user][0])
-	# 	max_buy = max(max_buy, buycnt[user][0])
-
-	# buyrate = {}
-	# #output buy rate
-
-	# alpha = 0.5
-	# for user in buycnt:
-	# 	buy_norm = buycnt[user][0]*1.0/(max_buy - min_buy)
-	# 	tot_norm = buycnt[user][1]*1.0/(max_tot - min_tot)
-	# 	buycnt_norm = (1 + alpha * alpha) * (buy_norm * tot_norm) / (alpha * alpha * buy_norm + tot_norm)
-	# 	buyrate[user] = buycnt_norm;
-	
-	# return buyrate
-	#fo.close()
 
 def cal_topK(buyrate, user_brandlist, output_file):
 	f = open(output_file, 'w')
@@ -82,6 +50,5 @@ if __name__ == "__main__":
 	data = load_data('../../data/user_brand_date_full.csv')
 	user_brandlist = load_user_brandlist('../data/sort_matrix.txt')
 	buyrate = cal_user_buyrate(data)
-
 	cal_topK(buyrate, user_brandlist, '../data/topk.txt')
 
