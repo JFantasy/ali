b2680165ef72baf77dc9d1d173398d72b92b1a1d
Zeyu Chen <zeyuchen@outlook.com>
2014-03-29 12:09:33 +0800

    optimize topk

--- pop/src/cal_topk.py
+++ pop/src/cal_topk.py
@@ -23,13 +23,13 @@ def load_user_brandlist(input_file):
 def cal_user_buyrate(data):
 	buycnt = {}
 	for r in data:
-		user,brand,behavior,month,day = r[0],r[1],r[2],int(r[3]),int(r[4])
+		user, brand, behavior, month, day = r[0],r[1],r[2],int(r[3]),int(r[4])
 		if user not in buycnt:
 			buycnt[user] = [0, 0, 0, 0, 0, 0]
 		
 		if behavior == '1':
 			buycnt[user][month-4] += 1
-			buycnt[user][5] += 1.0 / 5
+			buycnt[user][5] += 1
 
 	buyrate = {}
 	for user in buycnt:
@@ -41,14 +41,16 @@ def cal_topK(buyrate, user_brandlist, output_file):
 	f = open(output_file, 'w')
 	for user in user_brandlist:
 		list_len = len(user_brandlist[user])
-		topK = min(list_len, min(15, int(buyrate[user])*3))
+		topK = min(list_len, min(7, buyrate[user]))
+		if topK == 0:
+			topK = 1
 		f.write(user + ' ' + str(topK) + '\n')
 
 	f.close()
 
 if __name__ == "__main__":
 	data = load_data('../data/ali_order_brand_date_last_2_month.csv')
-	user_brandlist = load_user_brandlist('../data/sort_matrix.txt')
+	user_brandlist = load_user_brandlist('../data/sort_matrix_last_2_month.txt')
 	buyrate = cal_user_buyrate(data)
 	cal_topK(buyrate, user_brandlist, '../data/topk.txt')
 
